52218ab83afb05232c2260ebdae3d1b562c9d131
Angel85 <c185001@nwytg.net>
2021-08-23 20:42:43 -0300

    feature: Splash e iconos

--- index.js
+++ index.js
@@ -5,17 +5,33 @@ const menu = require('./menu');
 let window;
 
 app.on('ready', () => {
-    
+
     window = new BrowserWindow({
         width: 800,
         height: 600,
+        icon: __dirname + '/img/32x32.png',
+        show: false,
         webPreferences: {
             nodeIntegration: true
         }
     });
-    
+
+    splash = new BrowserWindow({ 
+        width: 400, 
+        height: 240,
+        resizable: false, 
+        frame: false, 
+        alwaysOnTop: true 
+    });
+
+    splash.loadFile('splash.html');
     window.loadFile('index.html');
-    
+
+    setTimeout(() => {
+        splash.destroy();
+        window.show();
+    }, 3000);
+
     autoUpdater.checkForUpdatesAndNotify();
 });
 
